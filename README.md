51999cea79b47ce53776c1df856efd1a391076a9
devCKVargas <vargaschrisking@gmail.com>
2023-11-14 18:31:13 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Apps install script (Windows Machine)
+
+## Requirements
+
+- **Internet connection**
+- **Admin rights**
+
+### App list
+
+## Chocolatey pkgs
+
+- wwinget-cli
+- .NET runtimes
+- Visual C++ runtimes
+- eartrumpet (Volume Control)
+- traffic-monitor (Network Speed monitor)
+- nerd-fonts-jetbrainsMono (font)
+- nerd-fonts-arimo (font)
+- nerd-fonts-meslo (font)
+- > snappy driver installer origin (BUGGED)
+
+## - Winget pkgs
+
+- git
+- github clig
+- github desktop
+- nodejs
+- terminal
+- powershell
+- powertoys
+- traffic monitor
+- caprine (Messenger)
+- discord
+- telegram
+- k-lite mega codec pack
+- obs studio
+- VLC media player
+- winrar
+- anydesk
+- gpu-z
+- f.lux
+- afterburner
+- nvcleanstall
+- superf4
+- wingetUI
+- Java JDK 18
+- 7-zip
+- Thorium Browser
+- CPU-Z version Taichi
---
+++ scripts/install_apps.ps1
@@ -0,0 +1,62 @@
+### > irm https://massgrave.dev/get | iex             # Microsoft Activation Scripts (massgrave.dev)
+### > iwr -useb https://christitus.com/win | iex      # WinUtil (ChrisTitusTech)
+
+### Winget (FIX! Slow Download)
+# `winget settings` add
+# "network": {
+#    "downloader": "wininet" // or "do" when (delivery optimization is enabled)
+# }
+
+### Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+### Install apps (chocolatey)
+choco install "winget" "dotnet-all" "vcredist-all" "eartrumpet" "traffic-monitor" "nerd-fonts-jetbrainsMono" "nerd-fonts-arimo" "nerd-fonts-meslo" "sdio" -y
+
+### Update current apps (winget)
+winget update --all -h --disable-interactivity ## --silent | -h
+
+### Install apps (winget)		# --source | -s (winget,msstore)
+winget install "git" "github cli" "github desktop" "lazygit" "nodejs" "terminal" "powershell" "powertoys" "traffic monitor" "caprine" "discord" "telegram" "k-lite mega codec pack" "obs studio" "VLC media player" "winrar" "anydesk" "gpu-z" "f.lux" "afterburner" "nvcleanstall" "superf4" "wingetUI" "oracle.JDK.18" "7-zip" "Alex313031.Thorium" --id "cpuidcpu-z.taichi" -s winget --accept-package-agreements --accept-source-agreements -h
+
+choco cache remove -y # clear chocolatey cache
+
+# App list
+### Chocolatey pkgs
+# "winget" (winget-cli)
+# "dotnet-all" (.NET runtimes)
+# "vcredist-all" (Visual C++ runtimes)
+# "eartrumpet" (Volume Control)
+# "traffic-monitor" (Network Speed monitor)
+# "nerd-fonts-jetbrainsMono" (font) 
+# "nerd-fonts-arimo" (font)
+# "nerd-fonts-meslo" (font)
+## > "realtek-hd-audio-driver" failed
+# "sdio" (snappy driver installer origin)
+### Winget pkgs
+# "git"
+# "github clig"
+# "github desktop"
+# "nodejs"
+# "terminal"
+# "powershell"
+# "powertoys"
+# "traffic monitor"
+# "caprine" (Messenger)
+# "discord"
+# "telegram"
+# "k-lite mega codec pack"
+# "obs studio"
+# "VLC media player"
+# "winrar"
+# "anydesk"
+# "gpu-z"
+# "f.lux"
+# "afterburner"
+# "nvcleanstall"
+# "superf4"
+# "wingetUI"
+# "oracle.JDK.18" (Java JDK 18)
+# "7-zip"
+# "Alex313031.Thorium" (Thorium Browser)
+# --id "cpuidcpu-z.taichi" (CPU-Z version Taichi)
